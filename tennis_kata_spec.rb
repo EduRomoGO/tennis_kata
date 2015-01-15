283540a4ffcb88a2ffb993658d6ed2237d5f7882
EduRomoGO <eduromogo@outlook.com>
2015-01-15 13:16:00 +0100

    refactor test

--- tennis_kata_spec.rb
+++ tennis_kata_spec.rb
@@ -2,14 +2,13 @@ require './tennis_kata'
 
 RSpec.describe "Tennis game" do
 
-  let(:packages_selector) {packages_selector = Packages_selector.new(Books_list_operations.new)}
+  let(:player1) { Player.new }
+  let(:player2) { Player.new }
+  let(:game) { Tennis_game.new player1, player2 }
 
   it "starts with two players having 0 points each" do
-    player1 = Player.new
-    player2 = Player.new
-    game = Tennis_game.new player1, player2
     expect(game.player1.points).to eq(0)
-    expect(game.player2.points).to eq(0) 
+    expect(game.player2.points).to eq(0)
   end
 
 end
