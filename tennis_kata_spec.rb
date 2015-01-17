a87dd3e2d5aa75358e07eeb47a95177bd9c46141
EduRomoGO <eduromogo@outlook.com>
2015-01-17 02:02:40 +0100

    red

--- tennis_kata_spec.rb
+++ tennis_kata_spec.rb
@@ -24,4 +24,12 @@ RSpec.describe "Tennis game" do
     expect(game.player2.points).to eq(0)
   end
 
+  it "scoreboard, after player one wins three points, is 40 - 0" do
+    game.score player1
+    game.score player1
+    game.score player1
+    expect(game.player1.points).to eq(30)
+    expect(game.player2.points).to eq(0)
+  end
+
 end
