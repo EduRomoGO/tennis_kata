3e031457b221126bee35f235003c8da98f5bed24
EduRomoGO <eduromogo@outlook.com>
2015-02-01 17:15:26 +0100

    refactor

--- game.rb
+++ game.rb
@@ -14,44 +14,47 @@ require 'pry'
 require './player'
 
 class Tennis_game 
+
   attr_accessor :player1
   attr_accessor :player2 
   attr_accessor :scoreboard
 
+  
   def initialize player1, player2
     @player1 = player1
     @player2 = player2
     @scoreboard = [0,0]
   end
 
+  
   def show_scoreboard
     scoreboard
   end
 
+  
   def score player
     player.points+=1
     update_scoreboard
   end
 
+  
   def update_scoreboard
-
     determine_player_scores player1
     determine_player_scores player2
-
-    scores_are_equal_to_40_points = (@scoreboard[0] == 40 and @scoreboard[1] == 40)
-
     points_difference_between_players = (player1.points - player2.points).abs
+    determine_scoreboard_if_deuce_winner_or_advantage points_difference_between_players
+  end
 
+  
+  def are_scores_equal_and_over_30_points?
     equal_scores = player1.points == player2.points
     scores_over_30 = (player1.points > 3 and player2.points > 3)
-
-    determine_scoreboard_if_deuce_winner_or_advantage equal_scores, scores_over_30, points_difference_between_players
-
+    equal_scores and scores_over_30
   end
 
 
-  def determine_scoreboard_if_deuce_winner_or_advantage equal_scores, scores_over_30, points_difference_between_players
-    if(equal_scores and scores_over_30)
+  def determine_scoreboard_if_deuce_winner_or_advantage points_difference_between_players
+    if are_scores_equal_and_over_30_points?
       update_scoreboard_with_deuce
     elsif at_least_one_player_over_40_points?
       if points_difference_between_players >=2
@@ -62,18 +65,14 @@ class Tennis_game
     end
   end
 
-  def players_scores_are_over_40_points
-    @scoreboard[0] == '+40' and @scoreboard[1] == '+40'
-  end
-
-  def one_player_over_40_points_and_the_other_under
-    @scoreboard[0] == '+40' or @scoreboard[1] == '+40'
-  end
 
   def at_least_one_player_over_40_points?
+    one_player_over_40_points_and_the_other_under = @scoreboard[0] == '+40' or @scoreboard[1] == '+40'
+    players_scores_are_over_40_points = @scoreboard[0] == '+40' and @scoreboard[1] == '+40'
     players_scores_are_over_40_points or one_player_over_40_points_and_the_other_under
   end
 
+  
   def determine_player_scores player
     case player.points
       when 0
@@ -89,11 +88,13 @@ class Tennis_game
     end
   end
 
+  
   def update_scoreboard_with_deuce
     @scoreboard[0] = 'Deuce'
     @scoreboard[1] = ''
   end
 
+  
   def determine_winner
     if player1.points > player2.points
       @scoreboard[0] = 'Winner'
@@ -104,6 +105,7 @@ class Tennis_game
     end
   end
 
+  
   def determine_the_player_with_advantage
     if player1.points > player2.points
       @scoreboard[0] = 'Advantage'
